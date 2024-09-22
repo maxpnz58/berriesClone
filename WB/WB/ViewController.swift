482b8c622b2e63aa54e7289d4546aa1c7a6c6071
Max Kuznetsov <kuzma-2000@mail.riu>
2024-09-22 15:35:20 +0300

    storyboard deleted

--- WB/WB/ViewController.swift
+++ WB/WB/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let navigationBarView = {
+        let block = UIScrollView()
+        block.backgroundColor = .white
+        block.translatesAutoresizingMaskIntoConstraints = false
+        return block
+    }()
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+        setupUI()
+        
+        setupLayout()
+            
         // Do any additional setup after loading the view.
     }
+}
 
-
+private extension ViewController {
+    func setupUI() {
+        view.backgroundColor = .red
+    }
+    
+    func setupLayout() {
+        
+    }
 }
 
