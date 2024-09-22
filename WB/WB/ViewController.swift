e2fc98a481d521571516888d7074c6cec297e18a
Max Kuznetsov <kuzma-2000@mail.riu>
2024-09-22 13:58:14 +0300

    starting project

---
+++ WB/WB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WB
+//
+//  Created by Max on 22.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
